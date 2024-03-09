be7af735074b030ed4fe3cb481c9b55dec6bd204
AuntL1ke <88139445+AuntL1ke@users.noreply.github.com>
2024-03-09 09:35:21 -0800

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            // В цій програмі є захист, користувач може вводити запити тільки з select і не зможе змінити чи завдати шкоди програмі
+
+            string conn = "Data Source=DESKTOP-JGNME93;Initial Catalog=Salings;Integrated Security=True;Connect Timeout=2";
+            SqlConnection connection = new SqlConnection(conn);
+            connection.Open();
+            Console.WriteLine("Connected");
+            string injection = "select";
+            while (true)
+            {
+                Console.Write("Enter a command: ");
+                string cmd = Console.ReadLine();
+                if(cmd.ToLower() == "exit")
+                {
+                    break;
+                }
+                if(cmd.ToLower().Contains(injection))
+                {
+                    using (SqlCommand command = new SqlCommand(cmd, connection))
+                    {
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.Write($"{reader.GetName(i),16}");
+                            }
+                            Console.WriteLine();
+                            Console.WriteLine(new string('-', 120));
+
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.Write($"{reader[i],16}");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("command prohibited");
+                }
+               
+            }
+
+            connection.Close();
+        }
+    }
+}
