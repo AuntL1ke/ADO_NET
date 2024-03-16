a4ea217d9113ef57c55153a0b8f4662203125a9c
AuntL1ke <88139445+AuntL1ke@users.noreply.github.com>
2024-03-16 08:28:12 -0700

    Add files via upload

---
+++ 03_HomeWork/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using _02_HomeWork.Model;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Program;
+
+namespace _03_HomeWork_Fix
+{
+    public partial class MainWindow : Window
+    {
+        Storoom db;
+
+        public MainWindow()
+        {
+            InitializeComponents();
+            db = new Storoom(ConfigurationManager.ConnectionStrings["connStoroom"].ConnectionString);
+        }
+
+        private void getProducts(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = db.GetAllProducts();
+        }
+
+        private void getSuppliers(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = db.GetAllSuppliers();
+        }
+
+        private void createProduct(object sender, RoutedEventArgs e)
+        {
+           
+            Product newProduct = new Product
+            {
+                Name = productNameTextBox.Text,
+                Type = productTypeTextBox.Text,
+                Quantity = int.Parse(productQuantityTextBox.Text),
+                Cost = int.Parse(productCostTextBox.Text),
+                SupplierId = int.Parse(productSupplierIdTextBox.Text) 
+            };
+
+           
+            db.CreateProduct(newProduct);
+
+         
+            dataGrid.ItemsSource = db.GetAllProducts();
+        }
+
+        private void createSupplier(object sender, RoutedEventArgs e)
+        {
+    
+            Supplier newSupplier = new Supplier
+            {
+                Name = supplierNameTextBox.Text 
+            };
+
+          
+            db.CreateSupplier(newSupplier);
+
+           
+            dataGrid.ItemsSource = db.GetAllSuppliers();
+        }
+
+
+        private void updateProduct(object sender, RoutedEventArgs e)
+        {
+         
+            Product selectedProduct = dataGrid.SelectedItem as Product;
+            if (selectedProduct != null)
+            {
+            
+                db.UpdateProduct(selectedProduct);
+
+                dataGrid.ItemsSource = db.GetAllProducts();
+            }
+        }
+
+        private void deleteProduct(object sender, RoutedEventArgs e)
+        {
+          
+            Product selectedProduct = dataGrid.SelectedItem as Product;
+            if (selectedProduct != null)
+            {
+               
+                db.DeleteProduct(selectedProduct.Id);
+       
+                dataGrid.ItemsSource = db.GetAllProducts();
+            }
+        }
+
+        private void updateSupplier(object sender, RoutedEventArgs e)
+        {
+        
+            Supplier selectedSupplier = dataGrid.SelectedItem as Supplier;
+            if (selectedSupplier != null)
+            {
+        
+                db.UpdateSupplier(selectedSupplier);
+            
+                dataGrid.ItemsSource = db.GetAllSuppliers();
+            }
+        }
+
+        private void deleteSupplier(object sender, RoutedEventArgs e)
+        {
+        
+            Supplier selectedSupplier = dataGrid.SelectedItem as Supplier;
+            if (selectedSupplier != null)
+            {
+             
+                db.DeleteSupplier(selectedSupplier.Id);
+         
+                dataGrid.ItemsSource = db.GetAllSuppliers();
+            }
+        }
+    }
+}
---
+++ 03_HomeWork/Storoom.cs
@@ -0,0 +1,149 @@
+﻿using _02_HomeWork.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+internal partial class Program
+{
+    public class Storoom
+    {
+        private SqlConnection connection;
+
+        public Storoom(string conString)
+        {
+            connection = new SqlConnection(conString);
+            connection.Open();
+            Console.WriteLine("Connected");
+        }
+
+        private void SetCommandParams(ref SqlCommand command, Product product)
+        {
+            command.Parameters.Add("@name", System.Data.SqlDbType.NVarChar).Value = product.Name;
+            command.Parameters.Add("@type", System.Data.SqlDbType.NVarChar).Value = product.Type;
+            command.Parameters.Add("@quantity", System.Data.SqlDbType.Int).Value = product.Quantity;
+            command.Parameters.Add("@cost_price", System.Data.SqlDbType.Int).Value = product.Cost;
+            command.Parameters.Add("@producer", System.Data.SqlDbType.Int).Value = product.SupplierId;
+        }
+
+        private void SetCommandParams(ref SqlCommand command, Supplier supplier)
+        {
+            command.Parameters.Add("@name", System.Data.SqlDbType.NVarChar).Value = supplier.Name;
+        }
+
+        public void CreateProduct(Product product)
+        {
+            string cmdText = @"insert into Products 
+                            values(@name,@type,@quantity,@cost_price,@producer,@price)";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            SetCommandParams(ref command, product);
+            command.ExecuteNonQuery();
+        }
+
+        public void CreateSupplier(Supplier supplier)
+        {
+            string cmdText = @"insert into Suppliers
+                            values(@name)";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            SetCommandParams(ref command, supplier);
+            command.ExecuteNonQuery();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            string cmdText = @"update Products
+                               set
+                                   Name = @name,
+                                   TypeProduct = @type,
+                                   Quantity = @quantity,
+                                   Cost = @cost_price,
+                                   SupplierId = @producer
+                               where Id = @id";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            SetCommandParams(ref command, product);
+            command.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = product.Id;
+            command.ExecuteNonQuery();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            string cmdText = @"delete from Products
+                               where Id = @id";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            command.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = id;
+            command.ExecuteNonQuery();
+        }
+
+        public void UpdateSupplier(Supplier supplier)
+        {
+            string cmdText = @"update Suppliers
+                               set
+                                   Name = @name
+                               where Id = @id";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            SetCommandParams(ref command, supplier);
+            command.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = supplier.Id;
+            command.ExecuteNonQuery();
+        }
+
+        public void DeleteSupplier(int id)
+        {
+            string cmdText = @"delete from Suppliers
+                               where Id = @id";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            command.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = id;
+            command.ExecuteNonQuery();
+        }
+
+        private List<Product> GetProductQuery(SqlCommand command)
+        {
+            List<Product> products = new List<Product>();
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                products.Add(new Product()
+                {
+                    Id = (int)reader[0],
+                    Name = (string)reader[1],
+                    Type = (string)reader[2],
+                    Quantity = (int)reader[3],
+                    Cost = (int)reader[4],
+                    SupplierId = (int)reader[5],
+                    SupplyDate = reader[6].ToString()
+                });
+            }
+            reader.Close();
+            return products;
+        }
+
+        private List<Supplier> GetSupplierQuery(SqlCommand command)
+        {
+            List<Supplier> suppliers = new List<Supplier>();
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                suppliers.Add(new Supplier()
+                {
+                    Id = (int)reader[0],
+                    Name = (string)reader[1]
+                });
+            }
+            reader.Close();
+            return suppliers;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            string cmdText = "select * from Products";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            return GetProductQuery(command);
+        }
+
+        public List<Supplier> GetAllSuppliers()
+        {
+            string cmdText = "select * from Suppliers";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            return GetSupplierQuery(command);
+        }
+    }
+}
